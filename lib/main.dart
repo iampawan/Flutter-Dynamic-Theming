411f71ea638c4d964d8d79bb8a705964e2056b75
Pawan Kumar <pawan221b@gmail.com>
2018-07-02 20:50:59 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//Using Bloc
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -55,3 +56,46 @@ class Bloc {
 }
 
 final bloc = Bloc();
+
+//Using Stateful
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   MyAppState createState() {
+//     return new MyAppState();
+//   }
+// }
+
+// class MyAppState extends State<MyApp> {
+//   bool darkTheme = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("App"),
+//         ),
+//         body: Center(),
+//         drawer: Drawer(
+//           child: ListView(
+//             children: <Widget>[
+//               ListTile(
+//                 title: Text("Dark Theme"),
+//                 trailing: Switch(
+//                   value: darkTheme,
+//                   onChanged: (changed) {
+//                     setState(() {
+//                       darkTheme = changed;
+//                     });
+//                   },
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//       theme: darkTheme ? ThemeData.dark() : ThemeData.light(),
+//     );
+//   }
+// }
